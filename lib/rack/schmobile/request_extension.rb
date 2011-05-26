1d7983f23d1a4740d904a66a2ec7d6f12f8bdc7a
Morten Primdahl <morten@zendesk.com>
2011-05-26 10:26:48 -0700

    Making the request functionality a module for easier include in tests

--- lib/rack/schmobile/request_extension.rb
+++ lib/rack/schmobile/request_extension.rb
@@ -1,10 +1,18 @@
-Rack::Request.class_eval do
-  def is_mobile?
-    Rack::Schmobile::Filters::CHAIN.each do |filter|
-      result = filter.call(self)
-      return result unless result.nil?
-    end
+module Rack
+  module Schmobile
+    module Request
+      def is_mobile?
+        Rack::Schmobile::Filters::CHAIN.each do |filter|
+          result = filter.call(self)
+          return result unless result.nil?
+        end
 
-    false
+        false
+      end
+    end
   end
+end
+
+Rack::Request.class_eval do
+  include Rack::Schmobile::Request
 end
